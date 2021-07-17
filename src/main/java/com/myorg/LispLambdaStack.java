1f1dc8b977298295913eb4539e2f0dfa5837a5a3
Eric Soppi <eric.soppi@gmail.com>
2021-07-17 14:07:32 -0700

    init

--- src/main/java/com/myorg/LispLambdaStack.java
+++ src/main/java/com/myorg/LispLambdaStack.java
@@ -1,8 +1,15 @@
 package com.myorg;
 
 import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.SecretValue;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.pipelines.CdkPipeline;
+import software.amazon.awscdk.pipelines.SimpleSynthAction;
+import software.amazon.awscdk.pipelines.StandardNpmSynthOptions;
+import software.amazon.awscdk.services.codepipeline.Artifact;
+import software.amazon.awscdk.services.codepipeline.actions.GitHubSourceAction;
+import software.amazon.awscdk.services.codepipeline.actions.GitHubTrigger;
 
 public class LispLambdaStack extends Stack {
     public LispLambdaStack(final Construct scope, final String id) {
@@ -12,6 +19,28 @@ public class LispLambdaStack extends Stack {
     public LispLambdaStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        final Artifact sourceArtifact = new Artifact();
+        final Artifact cloudAssemblyArtifact = new Artifact();
+
+        final CdkPipeline pipeline = CdkPipeline.Builder.create(this, "Pipeline")
+            .pipelineName("MyAppPipeline")
+            .cloudAssemblyArtifact(cloudAssemblyArtifact)
+            .sourceAction(GitHubSourceAction.Builder.create()
+                .actionName("GitHub")
+                .output(sourceArtifact)
+                .oauthToken(SecretValue.secretsManager("GITHUB_TOKEN_NAME"))
+                .trigger(GitHubTrigger.POLL)
+                .owner("soapdogg")
+                .repo("LispLambda")
+                .build())
+            .synthAction(SimpleSynthAction.standardNpmSynth(
+                StandardNpmSynthOptions.builder()
+                    .sourceArtifact(sourceArtifact)
+                    .cloudAssemblyArtifact(cloudAssemblyArtifact)
+                    .installCommand("npm install -g aws-cdk")
+                    .buildCommand("mvn package")
+                    .synthCommand("cdk synth")
+                    .build()))
+            .build();
     }
 }
