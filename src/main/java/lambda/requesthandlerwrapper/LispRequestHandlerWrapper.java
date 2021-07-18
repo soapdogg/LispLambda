aa93fcb54879e585e7841f7d3e5ac3f1c0285188
Eric Soppi <eric.soppi@gmail.com>
2021-07-18 14:32:29 -0700

    Moment of truth -- will it work?

--- src/main/java/lambda/requesthandlerwrapper/LispRequestHandlerWrapper.java
+++ src/main/java/lambda/requesthandlerwrapper/LispRequestHandlerWrapper.java
@@ -3,15 +3,26 @@ package lambda.requesthandlerwrapper;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import lambda.datamodels.AtomNode;
+import lambda.interpreter.Interpreter;
+import lambda.singleton.InterpreterSingleton;
 
 public class LispRequestHandlerWrapper implements RequestHandler<String, Void> {
 
+    private final Interpreter interpreter;
+
+    public LispRequestHandlerWrapper() {
+        interpreter = InterpreterSingleton.INSTANCE.getInterpreter();
+    }
+
     @Override
-    public Void handleRequest(String s, Context context) {
+    public Void handleRequest(String input, Context context) {
         LambdaLogger logger = context.getLogger();
 
-        logger.log("Body: " + s);
+        logger.log("input: " + input + "\n");
+
+        String result = interpreter.interpret(input);
+
+        logger.log("result: " + result + "\n");
 
         return null;
     }
