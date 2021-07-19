963610badd191f3acd35a81ce4135d365ff58388
Eric Soppi <eric.soppi@gmail.com>
2021-07-18 20:36:13 -0700

    Minor update to logic inside of literal token value end index determiner

--- src/main/java/lambda/determiner/LiteralTokenValueEndIndexDeterminer.kt
+++ src/main/java/lambda/determiner/LiteralTokenValueEndIndexDeterminer.kt
@@ -1,15 +1,18 @@
 package lambda.determiner
 
+import lambda.constants.TokenValueConstants
+
 class LiteralTokenValueEndIndexDeterminer {
     fun determineEndIndexOfLiteralTokenValue(
         word: String,
         startingPos: Int
     ): Int {
         val wordLength = word.length
-        val firstNonAlphaNumericIndex = IntRange(startingPos, wordLength - 1)
-            .firstOrNull { i: Int -> !Character.isAlphabetic(word[i].toInt())
-                && !Character.isDigit(word[i]) && word[i] != '+' && word[i] != '-' && word[i] != '*'}
-        val endIndex = firstNonAlphaNumericIndex ?: Integer.MAX_VALUE
+        val firstParenthesesIndex = IntRange(startingPos, wordLength - 1)
+            .firstOrNull { i: Int ->
+                word[i] == TokenValueConstants.OPEN_PARENTHESES || word[i] == TokenValueConstants.CLOSE_PARENTHESES
+            }
+        val endIndex = firstParenthesesIndex ?: Integer.MAX_VALUE
         return wordLength.coerceAtMost(endIndex)
     }
 }
