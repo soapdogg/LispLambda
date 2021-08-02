14b190c43cba696669476b26bbe91f68b3a8ebe1
Eric Soppi <eric.soppi@gmail.com>
2021-08-02 16:52:42 -0600

    Replace recursive call with stack in expression list length asserter

--- src/main/java/lambda/asserter/ExpressionListLengthAsserter.kt
+++ src/main/java/lambda/asserter/ExpressionListLengthAsserter.kt
@@ -2,10 +2,7 @@ package lambda.asserter
 
 import lambda.core.constants.FunctionLengthConstants
 import lambda.core.constants.FunctionNameConstants
-import lambda.core.datamodels.AtomNode
-import lambda.core.datamodels.ExpressionListNode
-import lambda.core.datamodels.NodeV2
-import lambda.core.datamodels.UserDefinedFunction
+import lambda.core.datamodels.*
 import lambda.core.exceptions.NotAListException
 
 class ExpressionListLengthAsserter(
@@ -18,51 +15,53 @@ class ExpressionListLengthAsserter(
         nodes: List<NodeV2>,
         userDefinedFunctions: Map<String, UserDefinedFunction>
     ) {
-        nodes.forEach { node ->
-            if (node is ExpressionListNode) {
-                val address = node.children[0]
-                if(address is AtomNode) {
-                    val addressValue = address.value
-                    functionLengthMap[addressValue]?.let {
-                        functionLengthAsserter.assertLengthIsAsExpected(
-                            addressValue,
-                            it,
-                            node
-                        )
-                    }
-                    minimumFunctionLengthMap[addressValue]?.let{
-                        functionLengthAsserter.assertLengthIsAtLeastMinimum(
-                            addressValue,
-                            it,
-                            node
-                        )
-                    }
-                    userDefinedFunctions[addressValue]?.let {
-                        functionLengthAsserter.assertLengthIsAsExpected(
-                            addressValue,
-                            it.formalParameters.size + 1,
-                            node
-                        )
-                    }
-                    if (addressValue == FunctionNameConstants.COND) {
-                        for (i in 1 until node.children.size - 1) {
-                            val condParameter = node.children[i]
-                            if (condParameter is AtomNode) {
-                                throw NotAListException("Error! cond parameter: ${condParameter.value} is not a list!${'\n'}")
-                            } else {
-                                functionLengthAsserter.assertLengthIsAsExpected(
-                                    FunctionNameConstants.COND,
-                                    FunctionLengthConstants.TWO,
-                                    condParameter
-                                )
-                            }
+        val stack = Stack<ExpressionListNode>()
+        nodes.filterIsInstance<ExpressionListNode>().forEach{
+            stack.push(it)
+        }
+        while (stack.isNotEmpty()) {
+            val node = stack.pop()
+            val address = node.children[0]
+            if(address is AtomNode) {
+                val addressValue = address.value
+                functionLengthMap[addressValue]?.let {
+                    functionLengthAsserter.assertLengthIsAsExpected(
+                        addressValue,
+                        it,
+                        node
+                    )
+                }
+                minimumFunctionLengthMap[addressValue]?.let{
+                    functionLengthAsserter.assertLengthIsAtLeastMinimum(
+                        addressValue,
+                        it,
+                        node
+                    )
+                }
+                userDefinedFunctions[addressValue]?.let {
+                    functionLengthAsserter.assertLengthIsAsExpected(
+                        addressValue,
+                        it.formalParameters.size + 1,
+                        node
+                    )
+                }
+                if (addressValue == FunctionNameConstants.COND) {
+                    for (i in 1 until node.children.size - 1) {
+                        val condParameter = node.children[i]
+                        if (condParameter is AtomNode) {
+                            throw NotAListException("Error! cond parameter: ${condParameter.value} is not a list!${'\n'}")
+                        } else {
+                            functionLengthAsserter.assertLengthIsAsExpected(
+                                FunctionNameConstants.COND,
+                                FunctionLengthConstants.TWO,
+                                condParameter
+                            )
                         }
                     }
                 }
-                assertLengthIsAsExpected(
-                    node.children,
-                    userDefinedFunctions
-                )
+                node.children.filterIsInstance<ExpressionListNode>().forEach{
+                    stack.push(it)
+                }
             }
         }
     }
